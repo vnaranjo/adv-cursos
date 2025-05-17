ef4ebd3b5f0e65185b934e3af4c8d6a69f3eb783
vnaranjo <nasavi4@gmail.com>
2025-05-17 13:23:14 -0600

    Customize

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,36 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'myStorageBucket',
+  name: 'arbovida',
   isDefault: true,
    access: (allow) => ({
-    'public/*': [
-        allow.guest.to(['read', 'write']),
+    'cursos/*': [
         allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-   })
-});
-
-export const secondaryStorage = defineStorage({
-  name: 'mySecondaryStorageBucket',
-   access: (allow) => ({
-    'backup_public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'backup_admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'backup_private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
 });
